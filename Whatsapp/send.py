f835cb1f91a3002983a134a8f55cc4f0d9f363f8
Rajesh <49854183+Rajesh-Manikandan@users.noreply.github.com>
2019-10-30 22:50:41 +0530

    Emptied

--- Whatsapp/send.py
+++ Whatsapp/send.py
@@ -1,9 +1,1 @@
-client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    body='Hello there!',
-    from_='whatsapp:+14155238886',
-    to='whatsapp:+918072739891'
-)
-
-print(message.sid)
